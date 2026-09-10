138b32198d3a892f0761b53fbae2d6acc79452da
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-09-10 14:34:42 +0300

    fix styling bug for filament

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Run migrations
 # The --force flag is needed for production environments
 php artisan migrate 
-
+npx tailwindcss@3 --input ./resources/css/filament/admin/theme.css --output ./public/css/filament/admin/theme.css --config ./resources/css/filament/admin/tailwind.config.js --minify
+php artisan view:clear
 # Start the main process (supervisord)
 exec "$@"
